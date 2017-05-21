e5637c8f6e6ee9d464061ae255b41c46ee7873a8
Iago Effting <iago.effting@gmail.com>
2017-05-21 18:30:34 -0300

    A User May Respond to Threads

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -28,7 +28,7 @@ class ReadThreadsTest extends TestCase
     /** @test */
     public function a_user_can_see_a_single_thread()
     {
-        $this->get('/threads/' . $this->thread->id)
+        $this->get($this->thread->path())
             ->assertSee($this->thread->title);
     }
 
@@ -38,7 +38,7 @@ class ReadThreadsTest extends TestCase
         factory('App\Reply')
             ->create(['thread_id' => $this->thread->id]);
 
-        $this->get('/threads/' . $this->thread->id)
+        $this->get($this->thread->path())
             ->assertSee($this->thread->body);
     }
 }
